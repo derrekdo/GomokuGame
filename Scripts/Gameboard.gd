0665902539ddebaa0d3dd6e285849c7c598842b1
Duy Nguyen <duyhoangnguyen2000@gmail.com>
2025-01-29 00:36:18 -0800

    camera zoom to tilemap and input event is now local to camera zoom

--- Scripts/Gameboard.gd
+++ Scripts/Gameboard.gd
@@ -5,9 +5,22 @@ export var grid: Resource
 # var a = 2
 # var b = "text"
 onready var board = $board
-
+onready var camera = $Camera2D
+func _ready():
+	# 
+	var viewport_size = get_viewport().size # [x, y]
+	var tile_rec = board.get_used_rect()
+	var tile_size = board.cell_size
+	var tilemap_size  = Vector2(
+		tile_rec.end.x - tile_rec.position.x,
+		tile_rec.end.y - tile_rec.position.y
+	)
+	
+	var level_size = Vector2(tile_size * tilemap_size)
+	print(level_size)
+	camera.set_zoom(level_size / viewport_size)
+	
 func _input(event):
 	if event is InputEventMouseButton:
-		var mouse_pos = get_viewport().get_mouse_position()
-		var tile_pos = board.map_to_world(board.world_to_map(mouse_pos))
-		print(tile_pos)
+		var tile_pos = board.map_to_world(board.world_to_map(board.make_input_local(event).position))
+		print(grid.grid_coords(tile_pos))
